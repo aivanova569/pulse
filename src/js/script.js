7c96c22fca277703447a82c482a6df76955dda52
Anastasiia Ivanova <aivanova569@bigmir.net>
2021-06-19 01:01:51 +0300

    Add modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -32,8 +32,24 @@ $(document).ready(function(){
           $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
         });
       });
-    };
+    }
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modals
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, .modal').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
 });
